a0807da48d9269cd2b1278c55de35a46b8b06a10
Massoud Nohra <massoud.nohra@net.usj.edu.lb>
2024-03-05 14:32:59 +0200

    Changes

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons,FontAwesome } from '@expo/vector-icons';
-import Home from './screens/Home';
-import Login from './screens/login-signup/login';
-import Signup from './screens/login-signup/singup';
-import Welcome from './screens/Welcome';
-import Settings from './screens/Settings';
-import Profile from './screens/Profile';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import Home from "./screens/Home";
+import Login from "./screens/login-signup/login";
+import Signup from "./screens/login-signup/singup";
+import Welcome from "./screens/Welcome";
+import Settings from "./screens/Settings";
+import Profile from "./screens/Profile";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -21,22 +21,29 @@ function BottomTabNavigation() {
           let iconName;
           let IconComponent = Ionicons;
 
-          if (route.name === 'Home') {
-            iconName = focused ? 'dashboard' : 'dashboard-outline';
+          if (route.name === "Dashboard") {
+            iconName = focused ? "dashboard" : "dashboard";
             IconComponent = FontAwesome;
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'settings' : 'settings-outline';
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
           }
 
           return <IconComponent name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: '#3090c9', 
-        tabBarInactiveTintColor: 'gray', 
+        tabBarActiveTintColor: "#3090c9",
+        tabBarInactiveTintColor: "gray",
       })}
     >
-      <Tab.Screen name="Home" component={Home}  />
-      <Tab.Screen name="Settings" component={Settings} options={{ headerShown: false}} />
-   
+      <Tab.Screen
+        name="Dashboard"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{ headerShown: false }}
+      />
     </Tab.Navigator>
   );
 }
@@ -44,12 +51,36 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="HomeStack" component={BottomTabNavigation} options={{ headerShown: false, headerLeft: () => null, }} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="SettingsStack" component={BottomTabNavigation} options={{headerLeft: () => null, }} />
-        <Stack.Screen name="Profile" component={Profile} options={{ headerShown:false,headerLeft: () => null, }} />
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomeStack"
+          component={BottomTabNavigation}
+          options={{ headerShown: false, headerLeft: () => null }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SettingsStack"
+          component={BottomTabNavigation}
+          options={{ headerLeft: () => null }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ headerShown: false, headerLeft: () => null }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
